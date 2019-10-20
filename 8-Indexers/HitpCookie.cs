33980c4ba9506367f7d3bf37610ee7b54892227c
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-21 02:16:10 +0700

    8-Indexers (AddClass)

--- 8-Indexers/HitpCookie.cs
+++ 8-Indexers/HitpCookie.cs
@@ -4,9 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _8_Indexers
+namespace Indexers
 {
-    class HitpCookie
+    public class HitpCookie
     {
+        private Dictionary<string, string> _dictionary;
+        public DateTime Expity { get; set; }
+        public HitpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+
+        }
+
+        public void SetItem(string key, string value)
+        {
+
+        }
+        public void GetItem(string key)
+        {
+
+        }
+
+
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
+        }
+
+
     }
 }
