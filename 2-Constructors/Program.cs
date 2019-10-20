6aa0a6435da57e8a85dc9e92eb24f0a7b9e4e00c
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-21 01:13:07 +0700

    2-Constructors

---
+++ 2-Constructors/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1, "khing");
+
+            var order = new Order();
+
+            customer.Order.Add(order);
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+        }
+    }
+}
