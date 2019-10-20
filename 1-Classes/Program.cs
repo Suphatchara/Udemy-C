e44db7b4a6817da3f509fd22408b0d7bdfcf8dd5
Suphatchara <suphatchara.wo@rmuti.ac.th>
2019-10-21 00:59:15 +0700

    1-Classes

--- 1-Classes/Program.cs
+++ 1-Classes/Program.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _1_Classes
+namespace Classes
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+
+        }
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var person = Person.Parse("John");
+            person.Introduce("Mosh");
         }
     }
 }
